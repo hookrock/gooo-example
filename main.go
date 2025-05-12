f7b4a022b25342dd2c224c9b22a32c637aeeb6c7
unknown <2240040604@qq.com>
2025-05-12 18:20:58 +0800

    h

--- main.go
+++ main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/hookrock/gooo"
 )
 
 func main() {
 	r := gooo.Default()
+	//添加认证
+	//r.Use(gooo.AuthMiddleware)
+	//实现认证路由看守器
+	//
 	r.GET("/", func(c *gooo.Context) {
 		c.View("index.tmpl", nil)
 	})
+	// 生成一个读取参数的get请求
+	r.GET("/get", func(c *gooo.Context) {
+		// name := c.Query("name") // 假设 Query 是获取查询参数的方法
+		// if name == "" {
+		// 	name = "Guest"
+		// }
+		name := c.GetParamWithDefault("name", "Guest")
+
+		message := fmt.Sprintf("hello world %s", name)
+		c.String(200, message)
+	})
+
 	r.Run()
 }
